5b4d5eef2915946f8b12bcfa664f3e3691ca0c4d
miller92time <sjmiller92@gmail.com>
2017-10-08 23:29:57 -0500

    Added some initial JFRAME code

--- Agile_Game/src/AgileGame.java
+++ Agile_Game/src/AgileGame.java
@@ -1,9 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
 
 public class AgileGame {
+	
+	//game variables
+	String gameString = "Agile Game";
+	int gameWidth = 1000;
+	int gameHeight = 1000;
+	static boolean gameStatus = true;
+	JFrame frame = new JFrame(gameString);
+	
+	
+	private void initializeGUI() {
+		//for initializing the initial gui
+		frame.setSize(gameWidth, gameHeight);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AgileGame game = new AgileGame();
+		game.initializeGUI();
+		
+		//game loop
+		while(gameStatus) {
+			/*
+			 * game loop
+			 * 1. process input
+			 * 2. update game state
+			 * 3. render (update graphics)
+			 */
+			
+		}
+		
 	}
 
 }
