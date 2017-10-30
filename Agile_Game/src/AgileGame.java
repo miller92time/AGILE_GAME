04d272fa31442c43d29fb9a553c9b6e47f7a8953
miller92time <sjmiller92@gmail.com>
2017-10-30 17:37:34 -0500

    Threading
    Changed game to threaded for the main loop

--- Agile_Game/src/AgileGame.java
+++ Agile_Game/src/AgileGame.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class AgileGame {
+public class AgileGame implements Runnable{
 
 	//game variables
 	final String gameString = "Agile Game";
@@ -17,7 +17,7 @@ public class AgileGame {
 	Team team2;
 
 	public AgileGame() {
-		
+
 		gameStatus = true;
 		frame = new JFrame(gameString);
 		dice = new Dice();
@@ -30,65 +30,116 @@ public class AgileGame {
 
 	public void addComponent(JComponent c) {
 		//used for adding a component to the frame
-		
+
 		frame.add(c);
 	}
 
 	public void drawComponents() {
 		//Refreshes and draws all current components in the game to the screen
-		
+
 		frame.revalidate();
-		frame.setBackground(Color.WHITE);
+		frame.getContentPane().setBackground(Color.BLACK);
 		frame.repaint();
-		
+
 	}
-	
-	
+
+
 	public void checkGameStatus() {
 		//game logic here
-		
+
 		//on click events
-		
-		
+
+
 		checkGameOver();
-		
-		
-		
+
+
+
 	}
-	
+
 	public void checkGameOver() {
 		//Checks for game over and exits game loop
-		
+
 		//game over logic
-		
+
 		//if - exit button then exit
-		
+
 		if(team1.getScore() >= 100) team1.setWin();
 		else if (team2.getScore() >= 100) team2.setWin();
-		
+
 		if(team1.getWin() || team2.getWin()) {
 			frame.removeAll();
 			//Graphics g = frame.getGraphics();
 			//frame.add(g);
 			gameStatus = false;
 		}
-			
-		
-		
-		
-		
+
+
+
+
+
 	}
 
 
 	private void initializeGUI() {
 		//for initializing the initial gui
 		frame.setSize(gameWidth, gameHeight);
-		frame.setBackground(Color.WHITE);
+		//frame.getContentPane().setBackground(Color.RED);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
-	
-	
+
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		AgileGame game = new AgileGame();
+		game.initializeGUI();
+		Deck deck = new Deck();
+		Card card = new Card(50,50);
+		game.addComponent(card);
+		game.drawComponents();
+		Card card2 = deck.draw();
+		card2.setY(50);
+		card2.setX(500);
+		game.addComponent(card2);
+		game.drawComponents();
+		Die die = new Die(300,300);
+		game.addComponent(die);
+		game.drawComponents();
+
+
+		while(gameStatus) {
+
+			//long currentTime = System.nanoTime();
+			/*
+			 * game loop
+			 * 1. process input
+			 * 2. update game state
+			 * 3. render (update graphics)
+			 * game.drawComponent();
+			 * (Use calls to drawCard and rollDice)
+			 */
+
+
+			game.checkGameStatus();
+			game.drawComponents();
+			game.team1.addToScore(101);
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ex)
+			{
+				ex.printStackTrace();
+			}
+			
+
+		}
+
+
+	}
+
+
+	/*
 
 	public static void main(String[] args) {
 
@@ -105,10 +156,10 @@ public class AgileGame {
 		game.addComponent(card2);
 		game.drawComponents();
 		Die die = new Die(300,300);
-		
-		
+
+
 		game.team1.drawCards();
-		
+
 
 
 		//need to add button for rolls
@@ -119,7 +170,7 @@ public class AgileGame {
 
 		//game loop
 		while(gameStatus) {
-			
+
 			//long currentTime = System.nanoTime();
 			/*
 			 * game loop
@@ -128,17 +179,17 @@ public class AgileGame {
 			 * 3. render (update graphics)
 			 * game.drawComponent();
 			 * (Use calls to drawCard and rollDice)
-			 */
-			
-			
+			 *
+
+
 			game.checkGameStatus();
 			game.drawComponents();
-			game.team1.addToScore(101);
-			game.frame.removeAll();
+			//game.team1.addToScore(101);
+			//game.frame.removeAll();
 
 		}
-		
+
 
 	}
-
-}
+			 */
+		}
