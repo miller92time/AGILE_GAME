13342e8c5c229f76a146086c2c108165d9061293
MadalinaCn <madalina.cn@gmail.com>
2017-11-12 23:50:31 -0600

    Add MainGame (again)

---
+++ src/main/java/MainGame.java
@@ -0,0 +1,11 @@
+
+public class MainGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AgileGameDemo.main(null);// for test only
+		//(new Thread(new AgileGameDemo())).start();
+	}
+
+}
